f11a6cccc11783243480ebea091e7116df391bd5
TreeIsLife2 <71183248+TreeIsLife2@users.noreply.github.com>
2021-03-31 11:02:57 +0200

    Initial creation of template-like

---
+++ interactions/wiki.js
@@ -0,0 +1,17 @@
+const logging = require('../util/logging.js');
+const Wiki = require('../util/wiki.js');
+const {limitLength, partialURIdecode, allowDelete} = require('../util/functions.js');
+
+/**
+ * Post a message with inline wiki links.
+ * @param {Object} interaction - The interaction.
+ * @param {import('discord.js').Client} interaction.client - The client of the interaction.
+ * @param {import('../util/i18n.js')} lang - The user language.
+ * @param {import('../util/wiki.js')} wiki - The wiki for the interaction.
+ * @param {import('discord.js').TextChannel} [channel] - The channel for the interaction.
+ */
+
+function wiki_slash()
+{
+
+};
